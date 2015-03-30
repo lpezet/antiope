c56016fd3df02fc1ca1c20340ce23057667ac505
lpezet <lpezet@gmail.com>
2015-03-29 21:23:57 -0600

    ApacheHttpClientNetworkIO added.

---
+++ antiope2-core/src/main/java/com/github/lpezet/antiope2/dao/http/apache/ApacheHttpClientNetworkIO.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.github.lpezet.antiope2.dao.http.apache;
+
+import com.github.lpezet.antiope2.dao.INetworkIO;
+import com.github.lpezet.antiope2.dao.http.IHttpNetworkIO;
+import com.github.lpezet.antiope2.dao.http.IHttpRequest;
+import com.github.lpezet.antiope2.dao.http.IHttpResponse;
+
+/**
+ * @author Luc Pezet
+ *
+ */
+public class ApacheHttpClientNetworkIO  implements IHttpNetworkIO<IHttpRequest, IHttpResponse> {
+
+	private IApacheHttpClientMarshaller mApacheMarshaller;
+	private IApacheHttpClientUnmarshaller mApacheUnmarshaller;
+	private INetworkIO<ApacheHttpRequest, ApacheHttpResponse> mApacheNIO;
+	
+	public ApacheHttpClientNetworkIO(IApacheHttpClientMarshaller pMarshaller, INetworkIO<ApacheHttpRequest, ApacheHttpResponse> pNIO, IApacheHttpClientUnmarshaller pUnmarshaller) {
+		mApacheMarshaller = pMarshaller;
+		mApacheNIO = pNIO;
+		mApacheUnmarshaller = pUnmarshaller;
+	}
+	
+	@Override
+	public IHttpResponse perform(IHttpRequest pRequest) throws Exception {
+		ApacheHttpRequest oRQ = mApacheMarshaller.perform(pRequest);
+		ApacheHttpResponse oRS = mApacheNIO.perform(oRQ);
+		IHttpResponse oResult = mApacheUnmarshaller.perform(oRS);
+		return oResult;
+	}
+}
